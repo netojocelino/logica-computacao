8fdddcf80679a3f8a7360e59752edda7dd4ff2d5
thiago-sandes <thiago-sandes@hotmail.com>
2018-01-04 11:21:48 -0200

    update...
    Explanations in group meeting.

--- trabalho.c
+++ trabalho.c
@@ -19,18 +19,21 @@
 char* string_substring(char str[], int start, int end);
 
 int main(){
-	char form[100], *substring;
+	char form[100], *substring, c;
 	char clausulas[32][32]; // Variável para separação de cláusulas e resolução
                                 // Ex: clausulas[0][0-(numero de caracteres da clausula]: {A,B,C}
                                 // clausulas[1][0 - ...]: {~A,~B,~C}
                                 // compara(como fazer?) clausulas [0][0-...] com clausulas [1][0-...]
                                 // resposta na próxima linha: clausulas[2][0-...] : {} CLÁUSULA VAZIA DECORRENTE DA COMPARAÇÃO DAS CLÁUSULAS ANTERIORES
-        int i, j, ini, pos=0;
-    	FILE *arquivo;
-    
+        int i, j, l = 0, k,  ini, pos=0;
+    	
+        
+        FILE *arquivo;
+        
     	arquivo = fopen("arquivo.txt", "w");
-    
-    
+        
+        printf("Instruções \n\n"); 
+        printf("Caractere maiúsculo significa valor verdade, caractere minúsculo significa valor falsidade. \n\n");
     	printf("Insira o conjunto de cláusulas na forma de conjuntos: ");
 
 	gets(form); 
@@ -50,35 +53,82 @@ int main(){
  	   	fprintf(arquivo, "%s\r\n", substring);
  
 		
-		//printf("A substring: %s\n", substring);
+		printf("A substring: %s\n", substring);
     	}
-    	
-	/* Solução através de matriz de caracteres
-	
+    
+        fclose(arquivo);
+        
+        // Caracteres em ASCII
+        // 65-90: Alfabeto maiúsculo
+        // 97-122: Alfabeto minúsculo
+        // 10: nova linha - Line Feed
+        
+        arquivo = fopen("arquivo.txt", "r");
+        
         for(i = 0; i < 32; i++){
+            k = 0;
+            
             for(j = 0; j < 32; j++){
-                // Codigo para ler linha por linha do arquivo e adicionar na matriz de caracteres
+                // Codigo para ler linha por linha e adicionar na matriz de caracteres
+                c = fgetc(arquivo);
                 
+                //printf("%c \n", c);
+                
+                if((c > 64 && c < 91) || (c > 96 && c < 123)){
+                    clausulas[l][k] = c ;
+                    k = k + 1;
+                }else{
+                
+                    if(c == 10){
+                        for ( k ; k < 32; k++)
+                            clausulas[l][k] = 30;
+                        
+                        l = l + 1;
+                       
+                        break;
+                    }else{
+                        if(c == -1){
+                            
+                            for( l ; l < 32; l++){
+                                for ( k ; k < 32; k++){
+                                    clausulas[l][k] = 30;
+                                }
+                                
+                                k = 0;
+                            
+                            }
+                            
+                            break;
+                        
+                        }
+                    }
+                    
+                }
             }
+            
+            if(feof(arquivo))
+                break;
+        
         }
         
 	for(i = 0; i < 32; i++){
             for(j = 0; j < 32; j++){
-                printf("%c", clausulas[i][j]);
-                //Imprimir caracteres linha por linha
+                if((clausulas[i][j] > 64 && clausulas[i][j] < 91) || (clausulas[i][j] > 96 && clausulas[i][j] < 123))
+                    printf("%c", clausulas[i][j]);   
             }
-	    printf("\n");
+            
+            if((clausulas[i][j] > 64 && clausulas[i][j] < 91) || (clausulas[i][j] > 96 && clausulas[i][j] < 123))
+                    printf("\n");
+            
         }
         
-	*/
-       
-    	
-	//usando fclose para fechar o arquivo
-	fclose(arquivo);
+        
+    	//usando fclose para fechar o arquivo
+    	fclose(arquivo);
 
 	/*printf("%d",strlen(form));*/
 
- exit(0);
+        exit(0);
 }
 
 char* string_substring(char str[], int start, int end) {
@@ -101,4 +151,5 @@ char* string_substring(char str[], int start, int end) {
      
      
     return sub;
+    
 }
