ea47e7c8c525ffed1c49f0cf9d4585469cacd553
thiago-sandes <thiago-sandes@hotmail.com>
2018-03-01 20:01:56 -0300

    Add files via upload

--- Prova/Q4.pl
+++ Prova/Q4.pl
@@ -1,23 +1,49 @@
+pai(a,b).
+pai(a,c).
+pai(b,d).
+pai(b,e).
+pai(c,f).
 
-progenitor(jose, A) :- A = joao ; A = ana.
-progenitor(maria, A) :- A = joao ; A = ana.
-progenitor(ana, A) :- A = helena ; A = joana.
-progenitor(joao, mario).
-progenitor(A, carlos) :- A = helena ; A = mario.
+sexo(A, m) :- A = a ; A = b ; A = d.
+sexo(A, f) :- A = c ; A = e ; A = f.
 
-sexo(A, m) :- A = jose ; A = joao ; A = mario ; A = carlos.
-sexo(A, f) :- A = ana ; A = maria ; A = helena ; A = joana.
+progenitor(a, A) :- A = b ; A = c.
+progenitor(e, A) :- A = b ; A = c.
+progenitor(c, A) :- A = f ; A = e.
+progenitor(b, d).
+progenitor(A, c) :- A = d ; A = f.
 
-irma(A, B) :- sexo(A, f), progenitor(N, A) , progenitor(N, B).
-irmao(A, B) :- sexo(A, m), progenitor(N, A) , progenitor(N, B).
 
-descendente(A, B) :- progenitor(B, A).
+avo(X, Y) :- sexo(X, m), progenitor(X, N), progenitor(N, Y).
+mae(X, Y) :- sexo(X, f), progenitor(X, Y).
+pai(X, Y) :- sexo(X, m), progenitor(X, Y).
+tio(X, Y) :- sexo(X, m), descendente(X, N), avo(N, Y).
 
-mae(A, B) :- sexo(A, f), progenitor(A, B).
-pai(A, B) :- sexo(A, m), progenitor(A, B).
 
-avo(A, B) :- sexo(A, m), progenitor(A, N), progenitor(N, B).
 
-tio(A, B) :- sexo(A, m), descendente(A, N), avo(N, B).
 
-primo(A, B) :- sexo(A, m) , avo(N, A), avo(N, B), progenitor(M, A), progenitor(P, B), irmao(M, P) ; irma(M, P).
+
+a) defina um predicado irmao(X,Y) que é verdade se X e Y são irmãos.
+
+irmao(X, Y) :- sexo(X, m), progenitor(N, X) , progenitor(N, Y).
+irma(X, Y) :- sexo(X, f), progenitor(N, X) , progenitor(N, Y).
+
+b) defina um predicado primo(X,Y) que é verdade se X e Y são primos.
+
+primo(X, Y) :- sexo(X, m) , avo(N, X), avo(N, Y), progenitor(M, X), progenitor(P, Y), irmao(M, P) ; irma(M, P).
+
+c) defina um predicado neto(X,Y) que é verdade se X é neto de Y.
+
+neto(X, Y) :- sexo(X, m), progenitor(Y, N), progenitor(N, X).
+
+
+d) defina um predicado descendente(X,Y) que é verdade se X é descendente de Y.
+
+descendente(X, Y) :- progenitor(Y, X).
+
+
+e) faça a árvore de busca (algoritmo da resolução) para cada um dos predicados que você criou.
+Basta expandir até achar a segunda solução (se houver).
+
+
+
